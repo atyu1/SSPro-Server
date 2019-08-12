68bcae25308a81b5920ba178eec9377f0e793cff
atyu1 <attila.sovak@gmail.com>
2019-08-12 13:42:08 +0200

    Add POST for Datapoints API

--- main.go
+++ main.go
@@ -3,23 +3,31 @@ package main
 import(
 	"net/http"
 	"log"
-	"github.com/atyu1/SSPro-Server/datapoints"
+	"github.com/atyu1/SSPro-Server/models" 
+	"github.com/atyu1/SSPro-Server/controllers" 
 	"github.com/julienschmidt/httprouter"
 )
 
 
 func main() {
 
-	var dataPointHandler = datapoints.DataPointHandler
-	var dataPointGet = datapoints.DataPointGet
+	//ToDo: Change to ENV or so
+	CONFIG_FILE := "github.com/atyu1/SSPro-Server/config.yaml"
+
+	config := datapoints.DbAccess{}
+	config.Init(CONFIG_FILE)
+
+	//sqlite3 used
+	datapoints.InitDb(config)
 
 	router := httprouter.New()
-	router.GET("/datapoints/:location", dataPointGet)
-	router.GET("/datapoints/:location/:room", dataPointGet)
-	router.GET("/datapoints/:location/:room/:name", dataPointGet)
+	//router.GET("/datapoints/:location", dataPointGet)
+	//router.GET("/datapoints/:location/:room", dataPointGet)
+	//router.GET("/datapoints/:location/:room/:name", dataPointGet)
 
-	router.POST("/datapoints", 
+	router.POST("/datapoints", controllers.CreateDataPoint)
 	
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+	datapoints.GetDb().Close()
 }
