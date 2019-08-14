cfc6aa46c3fed2f6b60b977c45966486ffc4b428
atyu1 <attila.sovak@gmail.com>
2019-08-14 15:37:12 +0200

    Fix and finish POST to DB

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-import(
-	"net/http"
-	"log"
-	"github.com/atyu1/SSPro-Server/models" 
-	"github.com/atyu1/SSPro-Server/controllers" 
+import (
+	"github.com/atyu1/SSPro-Server/controllers"
+	"github.com/atyu1/SSPro-Server/models"
 	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
 )
-
 
 func main() {
 
 	//ToDo: Change to ENV or so
-	CONFIG_FILE := "github.com/atyu1/SSPro-Server/config.yaml"
+	CONFIG_FILE := "./config.yaml"
 
 	config := datapoints.DbAccess{}
 	config.Init(CONFIG_FILE)
@@ -26,7 +25,6 @@ func main() {
 	//router.GET("/datapoints/:location/:room/:name", dataPointGet)
 
 	router.POST("/datapoints", controllers.CreateDataPoint)
-	
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 	datapoints.GetDb().Close()
