dfb03012b2e30eafb38652cd262775412fac6157
atyu1 <attila.sovak@gmail.com>
2019-11-02 09:11:00 +0100

    Fix password saving as bcrypt to db + log to glog

--- models/dbsqlite3.go
+++ models/dbsqlite3.go
@@ -1,10 +1,9 @@
 package models
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"log"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *gorm.DB
@@ -13,11 +12,15 @@ func InitDb(config DbAccess) {
 	var err error
 	db, err = gorm.Open("sqlite3", fmt.Sprintf("%s/%s", config.DbPath, config.DbName))
 	if err != nil {
-		log.Fatal("Connection to database failed!", fmt.Sprintf("%v", err))
+		glog.Fatalf("Connection to database failed! %v", err)
 	}
 
 	//ToDo test user, remove it later!
 	user := &User{Email: "test@test.com", Password: "test123#", Token: ""}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	user.Password = hashedPassword
+	glog.Infof("ToDo, remove this part later! Default user created: %v", user)
 
 	db.Debug().AutoMigrate(&Datapoint{})
 	db.Debug().AutoMigrate(&User{})
